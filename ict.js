d795ecc6557cf0c5ce9a969c36bbf993603b9825
kamalkunwar10 <kunwarbabukamal10@gmail.com>
2025-07-01 22:27:11 +0545

    Add files via upload

---
+++ ict.js
@@ -0,0 +1,13 @@
+function toggleMenu() {
+  const nav = document.getElementById('nav-links');
+  nav.classList.toggle('show');
+}
+function toggleMenu() {
+  const nav = document.getElementById('nav-links');
+  nav.classList.toggle('show');
+}
+
+document.getElementById('darkToggle').addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
+});
+ 
